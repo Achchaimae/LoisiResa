740cfecc3ead9385269b6154c14ec8c4a88f3d04
achchaimae <achchaimae1@gmail.com>
2024-01-27 15:06:57 +0100

    LOIS-8 LOIS-9 LOIS-10 LOIS-11 LOIS-12 crud activity Service �

---
+++ src/main/java/com/achchaimae/loisiresa/Domain/activity/ActivityService.java
@@ -0,0 +1,71 @@
+package com.achchaimae.loisiresa.Domain.activity;
+
+import com.achchaimae.loisiresa.Domain.activity.dto.ActivityReqDTO;
+import com.achchaimae.loisiresa.Domain.activity.dto.ActivityRespDTO;
+import com.achchaimae.loisiresa.Exception.RecordAlreadyExistsException;
+import com.achchaimae.loisiresa.Exception.ResourceNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ActivityService implements ActivityServiceInterface{
+
+    ActivityRepository activityRepository;
+    private final ModelMapper modelMapper;
+
+    public ActivityService(ModelMapper modelMapper, ActivityRepository activityRepository) {
+        this.activityRepository= activityRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    public List<ActivityRespDTO> getActivities(){
+        return activityRepository.findAll().stream()
+            .map(level -> modelMapper.map(level, ActivityRespDTO.class))
+            .collect(Collectors.toList());
+    }
+
+    public ActivityRespDTO findActivity(Integer activityId){
+        Optional<Activity> activityOptional= activityRepository.findById(activityId);
+        if(activityOptional.isPresent()){
+            Activity activity= activityOptional.get();
+            return  modelMapper.map(activity, ActivityRespDTO.class);
+        }
+        return null;
+    }
+
+    public ActivityRespDTO saveActivity(ActivityReqDTO activity){
+        if(activityRepository.existsById(activity.getId())){
+            throw new RecordAlreadyExistsException("Activity with Id "+activity.getId()+ "already exists.");
+        }
+        return modelMapper.map(activityRepository.save(modelMapper.map(activity, Activity.class)), ActivityRespDTO.class);
+    }
+
+    public ActivityRespDTO updateActivity(ActivityReqDTO activity, Integer id){
+        Optional<Activity> activityOptional= activityRepository.findById(id);
+        if(activityOptional.isPresent()){
+            Activity existActivity = activityOptional.get();
+            if(existActivity.getId()!=activity.getId() && activityRepository.existsById(activity.getId())){
+                throw new RecordAlreadyExistsException("Member with num " + activity.getId() + " already exists.");
+            }
+            activity.setId(Integer.valueOf(existActivity.getId()));
+            return  modelMapper.map(activityRepository.save(modelMapper.map(activity,Activity.class)), ActivityRespDTO.class);
+        }else {
+        throw new ResourceNotFoundException("Activity with id " + id + " not found.");
+        }
+    }
+
+    public Integer DeleteActivity(Integer activityId){
+        Optional<Activity> exist = activityRepository.findById(activityId);
+        if(exist.isPresent()){
+            activityRepository.deleteById(activityId);
+            return 1;
+        }
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/achchaimae/loisiresa/Domain/activity/ActivityServiceInterface.java
@@ -0,0 +1,4 @@
+package com.achchaimae.loisiresa.Domain.activity;
+
+public interface ActivityServiceInterface {
+}
